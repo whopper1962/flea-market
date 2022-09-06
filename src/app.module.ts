0b9a3d440fb4eb78d4503ddc0ae6056d6c205989
Masashi Kawakami <m_kawakami@avelio.co.jp>
2022-09-06 10:00:28 +0900

    implemented middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
 import { PlayersModule } from './players/players.module';
 import { TeamsModule } from './teams/teams.module';
+import { LoggerMiddleware } from './middleware/logger.middleware'
 
 @Module({
   imports: [ItemsModule, PlayersModule, TeamsModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure (consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes({
+      path: '/',
+      method: RequestMethod.GET
+    });
+  }
+}
